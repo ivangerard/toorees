7ffac3b5e71b987475745cdf67117b41115a83ac
ivangerard <ivan.gerard@hotmail.com>
2016-10-20 20:52:00 +0700

    Login,Register Succeed

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ var express = require('express')
 var cors = require('cors')
 var morgan = require('morgan');
 var methodOverride = require('method-override');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express()
+var flash = require('connect-flash');
 var port = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://localhost/toorees')
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+
+// passport
+var flash = require('connect-flash');
+var path = require('path')
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+//tutup passport
+
 app.use(cors())
 app.use(morgan('dev')); // parse application/json
 app.use(bodyParser.json({
@@ -17,6 +28,21 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+
+
+mongoose.connect('mongodb://localhost/toorees')
+require('./controllers/passport')(passport); // pass passport for configuration
+
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(session({
+    secret: 'ivangerardmagic'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 app.use(methodOverride());
 app.use('/api', require('./routes/api'))
 app.listen(port);
